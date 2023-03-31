aec044c3b681d1896da0ce693af5cb3289744916
LucioFurnari <luciofurnari99@gmail.com>
2023-03-31 15:54:59 -0300

    Remove defualt boilerplate, add Personal section component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import PersonalDetails from './components/PersonalDetails'
+import './styles/App.css'
 
 function App() {
-
   return (
     <>
+      <PersonalDetails />
     </>
   )
 }
