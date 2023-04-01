6947191b352c4881be38d9536f00ea54bebc3819
LucioFurnari <luciofurnari99@gmail.com>
2023-03-31 23:51:18 -0300

    Change div for section tag

--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -25,16 +25,16 @@ export default function PersonalDetails() {
   }
   return (
     <>
-    <div className="name-section">
+    <section className="name-section">
       {valid ? <h1>{input.name} </h1> : <input name="name" onChange={handleInput} value={input.name} placeholder="Name"></input> }
       {valid ? <h2>{input.profession} </h2> : <input name="profession" onChange={handleInput} value={input.profession} placeholder="Profession"></input>}
-    </div>
-    <div className="contact-section">
+    </section>
+    <section className="contact-section">
       {valid ? <p> {input.number} | </p> : <input name="number" onChange={handleInput} value={input.number} placeholder="Number"></input>}
       {valid ? <span><a href={input.email}>{input.email} </a> |</span>  : <input name="email" onChange={handleInput} value={input.email} placeholder="Email"></input>}
       {valid ? <span><a href={input.github}> Github </a> | </span>: <input name="github" onChange={handleInput} value={input.github} placeholder="Github"></input>}
       {valid ? <a href={'https://github.com/'+input.linkedin}> Linkedin </a> : <input name="linkedin" onChange={handleInput} value={input.linkedin} placeholder="Linkedin profile"></input>}
-    </div>
+    </section>
     <button onClick={handleValid}>{valid ? 'Edit' : 'Save'}</button>
     </>
   )
