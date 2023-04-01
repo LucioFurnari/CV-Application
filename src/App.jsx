b5fec9b4a80d035ea325d847a05178e7bdf76e69
LucioFurnari <luciofurnari99@gmail.com>
2023-03-31 23:50:58 -0300

    Add ExperienceSection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import PersonalDetails from './components/PersonalDetails'
+import ExperienceSection from './components/ExperienceSection'
 import './styles/App.css'
 
 function App() {
   return (
     <main>
       <PersonalDetails />
+      <ExperienceSection />
     </main>
   )
 }
