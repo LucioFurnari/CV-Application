d770621a24cf69e3921b4db49ac307ce01a651d3
LucioFurnari <luciofurnari99@gmail.com>
2023-04-01 22:51:49 -0300

    Update the component to create an array of sections to render

--- src/components/ExperienceSection.jsx
+++ src/components/ExperienceSection.jsx
@@ -7,20 +7,37 @@ export default function ExperienceSection() {
     endDate: '',
     description: '',
     list: [],
+    edit: false,
   })
-  const [valid, setValid] = useState(false);
+  const [sections, setSections] = useState([]);
+  // const [valid, setValid] = useState(false);
 
   const {company, position, list, description} = input
 
-  function handleInput(ev) {
-    const {name} = ev.target
-    setInput({
-      ...input,
-      [name]: ev.target.value,
-    })
+  function addSection() {
+    setSections([...sections, input])
   }
-  function handleValid() {
-    setValid(!valid);
+
+  function handleInput(ev) {
+    const {name, id, value} = ev.target
+    const newArr = sections.map((item, i) => {
+      if(i == id) {
+        return {...item, [name]: value}
+      } else {
+        return item
+      }
+    })
+    setSections(newArr)
+  }
+  function handleValid(index) {
+    const newArr = sections.map((item, i) => {
+      if(i === index) {
+        return {...item, edit: !item.edit}
+      } else {
+        return item
+      }
+    })
+    setSections(newArr)
   }
   function handleList() {
     setInput({
@@ -32,19 +49,31 @@ export default function ExperienceSection() {
   return(
     <>
     <h2>Experience</h2>
-    <section>
-      {valid ? <h3>{company}</h3> : <input name="company" value={company} placeholder="Company" onChange={handleInput}></input>}
-      {valid ? <p>{position}</p> : <input name="position" value={position} placeholder="Position" onChange={handleInput}></input>}
+    {
+    sections.map((item,i) => {
+      const {edit, company, position, list, description} = item
+      return(
+      <section key={i}>
+        {edit ? 
+        <h3>{company}</h3> :
+        <input name="company" value={company} placeholder="Company" onChange={handleInput} id={i}></input>}
+        {edit ? 
+        <p>{position}</p> :
+        <input name="position" value={position} placeholder="Position" onChange={handleInput} id={i}></input>}
       <ul>
         {
-        !valid 
+        !edit 
         &&
-        <><input name="description" placeholder="Description" onChange={handleInput} value={description}></input><button onClick={handleList}>Add</button></>
+        <><input name="description" placeholder="Description" onChange={handleInput} value={description} id={i}></input><button onClick={handleList}>Add</button></>
         }
         {list.map((item, i) => <li key={i}>{item}</li>)}
       </ul>
+    <button onClick={() => handleValid(i)}>{edit ? 'Edit' : 'Save'}</button>
     </section>
-    <button onClick={handleValid}>{valid ? 'Edit' : 'Save'}</button>
+        )
+      })
+    }
+    <button onClick={addSection}>Add</button>
     </>
   )
 }
