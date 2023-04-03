9af2ce55520cb306c3097e0c37e70201a08d5d55
LucioFurnari <luciofurnari99@gmail.com>
2023-04-03 12:20:51 -0300

    Refactor the component to add multiple sections

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -1,35 +1,59 @@
 import { useState } from "react";
 export default function EducationSection() {
-  const [input, setInput] = useState({
+  const input = {
     school: '',
     title: '',
     startDate: '',
     endDate: '',
-  })
-  const [valid, setValid] = useState(false);
+    edit: false,
+  }
+  const [sections, setSections] = useState([]);
   const {school, title, startDate, endDate} = input;
 
-  function handleInput(cev) {
-    const {value,name} = ev.target;
-    setInput({
-      ...input,
-      [name]: value,
-    })
+  function addSection() {
+    setSections([...sections, input])
   }
-  function handleValid() {
-    setValid(!valid);
+
+  function handleInput(ev) {
+    const {id, value, name} = ev.target;
+    const newArr = sections.map((item,i) => {
+      if (id == i) {
+        return {...item, [name]: value}
+      } else {
+        return item
+      }
+    })
+    setSections(newArr)
+  }
+  function handleValid(index) {
+    const newArr = sections.map((item,i) => {
+      if(index == i) {
+        return {...item, edit: !item.edit}
+      } else {
+        return item
+      }
+    })
+    setSections(newArr);
   }
 
   return (
     <>
     <h2>Education</h2>
-    <section>
-      {valid ? <p>{school}</p> : <input name="school" value={school} onChange={handleInput}></input>}
-      {valid ? <p>{title}</p> : <input name="title" value={title} onChange={handleInput}></input>}
-      {valid ? <p>{startDate}</p> : <input name="startDate" value={startDate} onChange={handleInput}></input>}
-      {valid ? <p>{endDate}</p> : <input name="endDate" value={endDate} onChange={handleInput}></input>}
-    </section>
-    <button onClick={handleValid}>{valid ? 'Edit' : 'Save'}</button>
+    {
+      sections.map((item,i) => {
+        const {school, title, startDate, endDate, edit} = item;
+        return(
+          <section key={i}>
+            {edit ? <p>{school}</p> : <input name="school" value={school} id={i} onChange={handleInput}></input>}
+            {edit ? <p>{title}</p> : <input name="title" value={title} id={i} onChange={handleInput}></input>}
+            {edit ? <p>{startDate}</p> : <input name="startDate" value={startDate} id={i} onChange={handleInput}></input>}
+            {edit ? <p>{endDate}</p> : <input name="endDate" value={endDate} id={i} onChange={handleInput}></input>}
+            <button onClick={()=> {handleValid(i)}}>{edit ? 'Edit' : 'Save'}</button>
+          </section>
+        )
+      })
+    }
+    <button onClick={addSection}>Add</button>
     </>
   )
 }
