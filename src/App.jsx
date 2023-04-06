efc8c6754ba6aec1fcdb7c6d0f36a86de01c0c20
LucioFurnari <luciofurnari99@gmail.com>
2023-04-05 21:11:09 -0300

    Add skillsection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import PersonalDetails from './components/PersonalDetails'
 import ExperienceSection from './components/ExperienceSection'
 import EducationSection from './components/EducationSection'
+import Skillsection from './components/Skillsection'
 import './styles/App.css'
 
 function App() {
   return (
     <main>
       <PersonalDetails />
+      <Skillsection />
       <ExperienceSection />
       <EducationSection />
     </main>
