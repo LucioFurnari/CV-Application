b235c3ba656dc68e718a3387cb1c67c7a4112d2c
LucioFurnari <luciofurnari99@gmail.com>
2023-04-05 23:42:47 -0300

    Add valid boolean state, and edit/save button

--- src/components/Skillsection.jsx
+++ src/components/Skillsection.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react"
+import '../styles/PersonalSection.css'
 export default function Skillsection() {
   const [list, setList] = useState([]);
   const [input, setInput] = useState('');
+  const [edit, setEdit] = useState(false);
   
   function handleInput(ev) {
     const {value} = ev.target;
@@ -11,18 +13,25 @@ export default function Skillsection() {
   function addSkill() {
     setList([...list, input])
   }
+
+  function handleValid() {
+    setEdit(!edit)
+  }
   return(
     <section className="skillList-container">
       <h2>Skills</h2>
-      <input onChange={handleInput}></input>
-      <ul>
-      {list.map((item, index) => {
+        <>
+        {!edit && <input onChange={handleInput}></input>}
+        <ul className="skill-list">
+        {list.map((item, index) => {
         return (
           <li key={index}>{item}</li>
         )
-      })}
-      </ul>
-    <button onClick={addSkill}>Add skill</button>
+        })}
+        </ul>
+        {!edit && <button onClick={addSkill}>Add skill</button>}
+        </>
+    <button onClick={handleValid}>{edit ? 'Edit' : 'Save'}</button>
     </section>
   )
 }
