9cbebf90a4cc68033ef60715f2e8b3971ec3a4b1
LucioFurnari <luciofurnari99@gmail.com>
2023-04-05 21:10:44 -0300

    Add icons from react icon librarie

--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react"
+import { TiMail, TiDevicePhone, TiSocialLinkedin } from "react-icons/ti";
+import { VscGithub } from "react-icons/vsc";
+import { IconContext } from "react-icons";
 import '../styles/PersonalSection.css'
 
 export default function PersonalDetails() {
@@ -35,13 +38,13 @@ export default function PersonalDetails() {
       <input name="profession" onChange={handleInput} value={profession} placeholder="Profession"></input>}
     </section>
     <section className="contact-section">
-      {valid ? <span> {number} | </span> :
+      {valid ? <span><IconContext.Provider value={{size: "1.5em"}}><TiDevicePhone /></IconContext.Provider> {number} | </span> :
       <input name="number" onChange={handleInput} value={number} placeholder="Number"></input>}
-      {valid ? <span><a href={email} target="_blank">{email} </a> |</span>  :
+      {valid ? <span><IconContext.Provider value={{size: "1.5em"}}><TiMail/></IconContext.Provider><a href={email} target="_blank">{email} </a> | </span>  :
       <input name="email" onChange={handleInput} value={email} placeholder="Email"></input>}
-      {valid ? <span><a href={'https://github.com/'+ github} target="_blank"> Github </a> | </span>: 
+      {valid ? <span><IconContext.Provider value={{size: "1.5em"}}><VscGithub/></IconContext.Provider><a href={'https://github.com/'+ github} target="_blank"> Github </a> | </span>: 
       <input name="github" onChange={handleInput} value={github} placeholder="Github"></input>}
-      {valid ? <span><a href={linkedin} target="_blank"> Linkedin </a> </span> :
+      {valid ? <span><IconContext.Provider value={{size: "1.5em"}}><TiSocialLinkedin/></IconContext.Provider><a href={linkedin} target="_blank"> Linkedin </a> </span> :
       <input name="linkedin" onChange={handleInput} value={linkedin} placeholder="Linkedin profile"></input>}
     </section>
     <section className="contact-section">
